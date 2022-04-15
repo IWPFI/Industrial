21ca017c1101abd8c8daabb261c8fee703d29b98
lWPFl <3141629907@qq.com>
2022-04-15 16:30:53 +0800

    配置信息处理
    业务逻辑

---
+++ Industrial/BLL/DataResult.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Industrial.BLL
+{
+    /// <summary>
+    /// Result transfer class
+    /// </summary>
+    public class DataResult<T>
+    {
+        /// <summary>
+        /// 状态
+        /// </summary>
+        /// <remarks>没有异常就返回true</remarks>
+        public bool State { get; set; }
+
+        /// <summary>
+        /// Gets or sets the message.
+        /// </summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        /// 数据
+        /// </summary>
+        public T Data { get; set; }
+    }
+
+    public class DataResult : DataResult<string> { }
+}
---
+++ Industrial/BLL/IndustrialBLL.cs
@@ -0,0 +1,39 @@
+﻿using Communication;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Industrial.BLL
+{
+    public class IndustrialBLL
+    {
+        //获取串口信息
+        public DataResult<SerialInfo> InitSerialInfo()
+        {
+            DataResult<SerialInfo> result = new DataResult<SerialInfo>();
+            result.State = false;
+
+            try
+            {
+                SerialInfo SerialInfo = new SerialInfo();
+                SerialInfo.PortName = ConfigurationManager.AppSettings["port"];
+                SerialInfo.BaudRate = int.Parse(ConfigurationManager.AppSettings["baud"].ToString());
+                SerialInfo.DataBit = int.Parse(ConfigurationManager.AppSettings["data_bit"].ToString());
+                SerialInfo.Parity = (System.IO.Ports.Parity)Enum.Parse(typeof(System.IO.Ports.Parity), ConfigurationManager.AppSettings["parity"].ToString(), true);
+                SerialInfo.StopBits = (System.IO.Ports.StopBits)Enum.Parse(typeof(System.IO.Ports.StopBits), ConfigurationManager.AppSettings["stop_bit"].ToString(), true);
+
+                result.State = true;
+                result.Data = SerialInfo;
+            }
+            catch (Exception ex)
+            {
+                result.Message = ex.Message;
+            }
+
+            return result;
+        }
+    }
+}
