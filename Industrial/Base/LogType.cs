d2288dda15e1fe74831820a874d26035826f3281
lWPFl <3141629907@qq.com>
2022-05-07 19:05:03 +0800

    日志列表

---
+++ Industrial/Base/LogType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Industrial.Base
+{
+    public enum LogType
+    {
+        /// <summary>
+        /// 信息
+        /// </summary>
+        Info = 0,
+        /// <summary>
+        /// 警告
+        /// </summary>
+        Warn = 1,
+        /// <summary>
+        /// 错误
+        /// </summary>
+        Fault = 2
+    }
+}
---
+++ Industrial/Model/logModel.cs
@@ -0,0 +1,44 @@
+﻿using Industrial.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Industrial.Model
+{
+    public class LogModel
+    {
+
+        /// <summary>
+        /// Gets or sets the row number.
+        /// </summary>
+        /// <remarks>行号</remarks>
+        public int RowNumber { get; set; }
+
+        /// <summary>
+        /// Gets or sets the device name.
+        /// </summary>
+        /// <remarks>设备名称</remarks>
+        public string DeviceName { get; set; }
+
+
+        /// <summary>
+        /// Gets or sets the log info.
+        /// </summary>
+        /// <remarks>状态</remarks>
+        public string LogInfo { get; set; }
+
+        /// <summary>
+        /// Gets or sets the message.
+        /// </summary>
+        /// <remarks>详细信息</remarks>
+        public string Message { get; set; }
+
+        /// <summary>
+        /// Gets or sets the log type.
+        /// </summary>
+        /// <remarks>日志类型</remarks>
+        public LogType LogType { get; set; }
+    }
+}
