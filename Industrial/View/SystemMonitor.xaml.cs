feb6b713f5a19e7ca9f280077a6dc0cc6e289e2b
lWPFl <3141629907@qq.com>
2022-05-08 21:51:14 +0800

    仿真平台拖放交互操作

--- Industrial/View/SystemMonitor.xaml.cs
+++ Industrial/View/SystemMonitor.xaml.cs
@@ -24,5 +24,65 @@ namespace Industrial.View
         {
             InitializeComponent();
         }
+        /// <summary>
+        /// 鼠标是否按下
+        /// </summary>
+        bool _isMoving = false;
+
+        /// <summary>
+        /// 鼠标二维坐标
+        /// </summary>
+        Point _downPoint = new Point(0, 0);
+        double left = 0, top = 0;
+
+        //鼠标移动事件
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_isMoving)
+            {
+                Point currentPoint = e.GetPosition(sender as Canvas);
+
+                this.mainView.SetValue(Canvas.LeftProperty, left + (currentPoint.X - _downPoint.X));
+                this.mainView.SetValue(Canvas.TopProperty, top + (currentPoint.Y - _downPoint.Y));
+
+                e.Handled = true;
+            }
+        }
+
+        private void Canvas_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            _isMoving = false;
+            (sender as Canvas).ReleaseMouseCapture();
+            e.Handled = true;
+        }
+
+
+        private void Canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _isMoving=true;
+            _downPoint = e.GetPosition(sender as Canvas);
+
+            left = double.Parse(this.mainView.GetValue(Canvas.LeftProperty).ToString());
+            top = double.Parse(this.mainView.GetValue(Canvas.TopProperty).ToString());
+
+            (sender as Canvas).CaptureMouse();
+            e.Handled = true;
+        }
+
+        
+        // 缩放
+        private void Canvas_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            double newWidth = this.mainView.ActualWidth + e.Delta;
+            double newHeight = this.mainView.ActualHeight + e.Delta;
+
+            if (newWidth < 500) newWidth = 500;
+            if (newHeight < 100) newHeight = 100;
+
+            this.mainView.Width = newWidth;
+            this.mainView.Height = newHeight;
+
+            this.mainView.SetValue(Canvas.LeftProperty, (this.RenderSize.Width - this.mainView.Width) / 2);//根据中心位置进行缩放
+        }
     }
 }
